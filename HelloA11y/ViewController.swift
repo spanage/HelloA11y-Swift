cc1fd5adc981db4bc3f8b7c0ea80673c1706ce15
Sommer Panage <spanage@Sommers-MacBook-Pro.local>
2017-05-04 18:18:16 -0700

    initial project commit

---
+++ HelloA11y/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloA11y
+//
+//  Created by Sommer Panage on 5/4/17.
+//  Copyright © 2017 Sommer Panage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

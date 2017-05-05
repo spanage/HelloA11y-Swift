5a1e6f48679a5761e0315bae84b510f6cd36c09a
Sommer Panage <spanage@Sommers-MacBook-Pro.local>
2017-05-04 22:24:10 -0700

    main VC

--- HelloA11y/MainViewController.swift
+++ HelloA11y/MainViewController.swift
@@ -6,12 +6,29 @@
 //  Copyright © 2017 Sommer Panage. All rights reserved.
 //
 
-import UIKit
+import Cartography
 
 final class MainViewController: UIViewController {
     
+    fileprivate let items = MainItem.all
+    
+    private let tableView: UITableView = {
+        let view = UITableView(frame: .zero, style: .plain)
+        view.backgroundColor = .white
+        view.separatorStyle = .none
+        return view
+    }()
+    
     init() {
         super.init(nibName: nil, bundle: nil)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
+        constrain(tableView, view) { table, parent in
+            table.edges == parent.edges
+        }
     }
 
     
@@ -23,18 +40,68 @@ final class MainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let paper = Paper(frame: CGRect(x: 0, y: 0, width: 300, height: 400))
-        paper.fill = "cccccc"
-        let c = paper.circle(xCenter: 100, yCenter: 100, radius: 50)
-        c.fill = "ff0000"
-        c.stroke = c.fill
-        
-        view.addSubview(paper)
+        view.addSubview(tableView)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+}
+
+extension MainViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = MainTableViewCell()
+        cell.item = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return tableView.bounds.width
+    }
+}
+
+enum MainItem {
+    case numbers
+    case colors
+    case shapes
+    
+    static let all: [MainItem] = [.numbers, .colors, .shapes]
+    
+    var name: String {
+        switch self {
+        case .numbers: return "Learn Numbers"
+        case .colors: return "Learn Colors"
+        case .shapes: return "Learn Shapes"
+        }
+    }
+    
+    var image: UIImage {
+        switch self {
+        case .numbers: return #imageLiteral(resourceName: "calculator")
+        case .colors: return #imageLiteral(resourceName: "rainbow")
+        case .shapes: return #imageLiteral(resourceName: "shapes")
+        }
+    }
+    
+    var color: UIColor {
+        switch self {
+        case .numbers: return .cyan
+        case .colors: return .magenta
+        case .shapes: return .orange
+        }
+    }
+    
+    var selectionColor: UIColor {
+        switch self {
+        case .numbers: return .orange
+        case .colors: return .cyan
+        case .shapes: return .magenta
+        }
     }
 }
 
